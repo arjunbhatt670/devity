89e6ea02a84f8a6b2248b3dc7f9ecd3f1cf41de4
arjun.bhatt <arjun.bhatt@sprinklr.com>
2024-09-18 11:34:37 +0530

    feat(video): added video player

---
+++ src/app/[locale]/(unauth)/video-player/page.tsx
@@ -0,0 +1,10 @@
+import Video from './Video';
+
+export default function VideoPlayer() {
+  return (
+    <>
+      <p>Video Player</p>
+      <Video src="https://www.w3schools.com/html/mov_bbb.mp4" />
+    </>
+  );
+}
